01621f9d96fcf754068c2c832f1fdbf86980de55
Zach Bialecki <zach.bialecki@gmail.com>
2020-08-18 16:35:12 -0400

    Update pages/index.js to show issue

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,24 @@
 import Head from 'next/head'
 
 export default function Home() {
+  const title = 'Test Case for Issue #10183'
+  const authorNames = ['Alice', 'Bob', 'Carol', 'Dave']
+
   return (
     <div>
       <Head>
-        <title>This page has a title 🤔</title>
+        <title>{title}</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+
+        <meta name="citation_title" content={title} />
+        {authorNames.map(author => (
+          <meta key={author} name="citation_author" content={author} />
+        ))}
       </Head>
 
-      <h1>This page has a title 🤔</h1>
+      <h1>{title}</h1>
+      <h3>This page only has 1 meta tag with the name "citation_author" 🤔</h3>
     </div>
   )
 }
